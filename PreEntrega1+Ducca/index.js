54df6ede484643088bb9c12744b941e597a50a2d
GinaDucca95 <145928252+GinaDucca95@users.noreply.github.com>
2023-12-08 17:37:28 -0300

    Add files via upload

---
+++ PreEntrega1+Ducca/index.js
@@ -0,0 +1,21 @@
+window.prompt ("Para ingresar a la fila virtual escribi ok");
+
+for (var i=0;i<10;i++) {
+    console.log("Tu lugar en la fila virtual es " + i);
+}
+{
+    let nombreIngresado   = prompt("Ingresar nombre")
+    alert("El nombre ingresado es " + nombreIngresado)
+} 
+
+
+var totalEntrada = 2800 + 1300;
+var totalServicio = 900 + 650;
+
+var total = totalEntrada + totalServicio;
+
+console.log("El total de la entrada al Grand Prix es: " + totalEntrada);
+console.log("El total de la tarifa de servicio es: " + totalServicio);
+console.log("El precio final de la compra es: " + total);
+
+
